bc099f6a6e43f224c45c683abfdeafa1b85b3b97
lina <chelaifalina@gmail.com>
2025-02-22 19:10:56 +0100

    last

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import './App.css'
 
 import * as React from "react";
 
-const title = "React"; // Define variable outside the component
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>Hello {getTitle("React")}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
 
 export default App;
+
 
